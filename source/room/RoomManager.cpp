a5613b5604bb46f667a06f8cdaa13acd091595fd
Steve <steven.g.ball@gmail.com>
2016-05-22 21:30:35 -0400

    Better room generation params

--- source/room/RoomManager.cpp
+++ source/room/RoomManager.cpp
@@ -215,6 +215,8 @@ Room* RoomManager::CreateRandomRoom(Room* pRoomConnection, eDirection connectedD
 	int numRoomTries = 0;
 	while(overlapsExistingRoom == true && numRoomTries < 1)
 	{
+		roomLength = GetRandomNumber(50, 140, 2) * 0.1f;
+		roomWidth = GetRandomNumber(50, 140, 2) * 0.1f;
 		roomHeight = 1.0f;
 
 		*randomLengthOffset = GetRandomNumber(-100, 100, 2) * 0.01f;
